ca54b6ff8bf68281afef5b4f7234086fe1830822
ParkDongseok <76927397+pds0309@users.noreply.github.com>
2021-04-02 13:24:04 +0900

    1주차 끝

---
+++ src/main/java/com/sparta2/springcore/SpringcoreApplication.java
@@ -0,0 +1,15 @@
+package com.sparta2.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing // 시간 자동 변경이 가능하도록 합니다.
+@ServletComponentScan // Controller Service Repositiory 사용하지 않기 위해
+public class SpringcoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcoreApplication.class, args);
+    }
+}
